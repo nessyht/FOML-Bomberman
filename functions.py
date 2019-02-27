de8e8475f0db5fdd57ef3102b421f2d9d7263b1b
Karl Thyssen <thyssenkarl@gmail.com>
2019-02-27 13:18:30 +0100

    filled state vector

--- functions.py
+++ functions.py
@@ -13,10 +13,11 @@ def create_state_vector(self):
     ''' Create empty arena with 3D vector per cell for state information
         [Agent present: -1 for enemy, 0 for none, 1 for self;
          Coin or crate present: -1 for crate, 0 for none, 1 for coin;
-         bomb or explosion present: 0 for none, t for timer]
+         bomb or explosion present: 5 for none, t for timer]
     '''
-    state = np.zeros((arena.shape[0], arena.shape[1], 3))
-    
+    agent_state = np.zeros((arena.shape))
+    #loot_state = np.zeros((arena.shape))
+    #bomb_state = np.zeros((arena.shape))
     x, y, _, bombs_left = self.game_state['self']
     bombs = self.game_state['bombs']
     bomb_xys = [(x,y) for (x,y,t) in bombs]
@@ -26,64 +27,63 @@ def create_state_vector(self):
     explosions = self.game_state['explosions']
 
                 
-    extras = np.array([])
+    extras = np.zeros((1))
     
     # For every cell: 
 
     # Agent on cell, self
-    state[x, y, 0] = 1
+    agent_state[x, y] = 1
     
     # Agent on cell, enemy
     for o in others:
-        state[o[0], o[1], 0] = -1
+        agent_state[o[0], o[1]] = -1
         
     # Crate on cell? 
-    state[:, :, 1] = np.where(arena == 1, -1, 0)
+    loot_state = np.where(arena == 1, -1, 0)
             
     # Coin on cell?
     for coin in coins:
-        state[coin[0], coin[1], 1] = 1
-   
-    # Not functional yet
+        loot_state[coin[0], coin[1]] = 1
     
     # Bomb radius on cell?    
-
+    bomb_state = np.ones(arena.shape) * 5
     for xb,yb,t in bombs:
         for (i,j) in [(xb+h, yb) for h in range(-3,4)] + [(xb, yb+h) for h in range(-3,4)]:
-            if (0 < i < state.shape[0]) and (0 < j < state.shape[1]):
-                state[i,j, 2] = min(state[i,j, 2], t)
-    # not yet functional
+            if (0 < i < bomb_state.shape[0]) and (0 < j < bomb_state.shape[1]):
+                bomb_state[i,j] = min(bomb_state[i,j], t)
     
     # Explosion on cell? Important?
     
-    # Danger level
-    
+
     # Only once:
     # Current step
     
+    # Danger level
+    extras[0] = 5 - bomb_state[x, y]
     
     # Bomb action possible?
     
+    # Reward for step
+    # extras[1] = self.rewards[-1]
     
-    # Danger level for the agent
+    # Step number
     
     
-    # Agent touches opponent
+    # Reward for episode (added later)
+    
     
     # State of each cell
-    # state = np.reshape(arena, (arena.shape[0]*arena.shape[1],)) # reshape arena into 1D-array
-    state.flatten()
-    walls = np.argwhere(arena == -1)
-    offset = arena.shape[0]
-    indexes = np.concatenate((3*np.add(walls[:,0]*offset, walls[:,1]), 3*np.add(walls[:,0]*offset, walls[:,1]+1), 3*np.add(walls[:,0]*offset, walls[:,1]+2)))
-    # Indexes correct confirmed
-    state = np.mgrid[0:17, 0:17, 0:3].flatten()
-    vector = np.delete(state, indexes, axis=0).flatten() # delete walls. Those are the same everytime. Thus, we save memory
-    vector = np.concatenate((extras, vector)) # combine extras and state vector
-    print(state[0:17], state[state%17==0])
-    print(vector)
-    print(np.argwhere(arena == -1))
-    print(3*np.add(walls[18,0]*offset, walls[18,1]))
+    # combine state maps and flatten into 1D-array
+    state = np.stack((agent_state[arena != -1].flatten(), \
+                      loot_state[arena != -1].flatten(), \
+                      bomb_state[arena != -1].flatten()), \
+                      axis=-1).flatten()
+    
+    vector = np.concatenate((extras, state)) # combine extras and state vector
+    # print(state[0:17], state[state%17==0])
+    # print(vector)
+    #print(3*np.add(walls[18,0]*offset, walls[18,1]))
+    
     print(state.shape, vector.shape, 17*17*3, 176*3)
  
     
