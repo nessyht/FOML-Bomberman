ff4638fd5a8f62e696bb08375a909990436ff402
Hein <hein-erik.schnell@t-online.de>
2019-03-02 16:50:47 +0100

    updated functions

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def main():
 
     # Initialize environment and agents
     world = BombeRLeWorld([
-            ('user_agent', True),
+            ('simple_agent', True),
             ('simple_agent', False),
             ('simple_agent', False),
             ('simple_agent', False)
